520388091996a79ed64a0c7825aeb647f27d69b3
Angelina <gellaproios@gmail.com>
2022-02-13 19:34:14 +0700

    The color creation project is ready.

--- CreateColor/ViewController.swift
+++ CreateColor/ViewController.swift
@@ -9,11 +9,38 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var colorView: UIView!
+    @IBOutlet var valueRedLabel: UILabel!
+    @IBOutlet var valueGreenLabel: UILabel!
+    @IBOutlet var valueBlueLabel: UILabel!
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        colorView.layer.cornerRadius = 10
+        
+        valueRedLabel.text = String(redSlider.value)
+        valueGreenLabel.text = String(greenSlider.value)
+        valueBlueLabel.text = String(blueSlider.value)
+        
+    }
+    
+    func getColor() {
+        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
     }
 
-
+    func getValue () {
+        valueRedLabel.text = String(format: "%.2f", redSlider.value)
+        valueGreenLabel.text = String(format: "%.2f", greenSlider.value)
+        valueBlueLabel.text = String(format: "%.2f", blueSlider.value)
+    }
+    
+    @IBAction func allColorSlider() {
+        getColor()
+        getValue()
+    }
+    
 }
-
