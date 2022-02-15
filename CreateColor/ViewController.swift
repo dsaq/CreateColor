e324993ce8c8a0f5e44ecceb165e78ee562b95dc
Angelina <gellaproios@gmail.com>
2022-02-15 23:36:43 +0700

    Переделанный проект после просмотра разбора дз

--- CreateColor/ViewController.swift
+++ CreateColor/ViewController.swift
@@ -10,9 +10,11 @@ import UIKit
 class ViewController: UIViewController {
 
     @IBOutlet var colorView: UIView!
+    
     @IBOutlet var valueRedLabel: UILabel!
     @IBOutlet var valueGreenLabel: UILabel!
     @IBOutlet var valueBlueLabel: UILabel!
+    
     @IBOutlet var redSlider: UISlider!
     @IBOutlet var greenSlider: UISlider!
     @IBOutlet var blueSlider: UISlider!
@@ -20,27 +22,62 @@ class ViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        colorView.layer.cornerRadius = 10
         
-        valueRedLabel.text = String(redSlider.value)
-        valueGreenLabel.text = String(greenSlider.value)
-        valueBlueLabel.text = String(blueSlider.value)
+        colorView.layer.cornerRadius = 15
         
-    }
-    
-    func getColor() {
-        colorView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
-    }
-
-    func getValue () {
-        valueRedLabel.text = String(format: "%.2f", redSlider.value)
-        valueGreenLabel.text = String(format: "%.2f", greenSlider.value)
-        valueBlueLabel.text = String(format: "%.2f", blueSlider.value)
-    }
-    
-    @IBAction func allColorSlider() {
         getColor()
-        getValue()
+        
+        setValue(for: valueBlueLabel, valueRedLabel, valueGreenLabel)
+        
+    }
+/*
+ В качестве значения для сопоставления поставим sender. Sender - это параметр, который принимает тот элемент интерфейса, с которым на данный момент взаимодействует пользователь
+*/
+    
+    @IBAction func getColor(_ sender: UISlider) {
+        getColor()
+        
+        switch sender {
+        case redSlider:
+            valueRedLabel.text = string(from: redSlider)
+        case greenSlider:
+            valueGreenLabel.text = string(from: greenSlider)
+        default:
+            valueBlueLabel.text = string(from: blueSlider)
+        }
+    }
+    
+
+    private func getColor () {
+        colorView.backgroundColor = UIColor(
+            red: CGFloat(redSlider.value),
+            green: CGFloat(greenSlider.value),
+            blue: CGFloat(blueSlider.value),
+            alpha: 1
+        )
+    }
+    
+    // метод, принимающий [вариантивные параметры]. В него закинем все слайдеры. Чтобы обновлять значние лэйбла слайдера по необходимости
+    /* При использовании вариантивного параметра все значения, по сути, помещаются в массив. По этому пишем for label[s]. Раз это массив, то его нужно перебрать. Перебираем с помощью функции forEach (заменят for..in.. по сути такая же функция, только пишется в одну строку с помощью блоков замыкания). Берем значение из слайдера и передаем в этот лейбл, в зависимости от того, какой лейбл. Для этого используем switch.
+     Switch - передаем аутлеты и смотрим, если у нас этот лэбл является аутлетом redLabel, то передаем в redLabel.text результат функции string
+    */
+    
+    private func setValue(for labels: UILabel...) {
+        labels.forEach { label in
+            switch label {
+            case valueRedLabel:
+                valueRedLabel.text = string(from: redSlider)
+            case valueGreenLabel:
+                valueGreenLabel.text = string(from: greenSlider)
+            default:
+                valueBlueLabel.text = string(from: blueSlider)
+            }
+            
+        }
+    }
+    
+    private func string(from slider: UISlider) -> String {
+        String(format: "%.2f", slider.value)
     }
     
 }
